10c4b43ca9bf6b3e4d3271c4f4f77d29015872b5
Nishal Barman <helptechnoapps@gmail.com>
2023-10-05 14:36:24 +0530

    Realtime chat added using socket.io

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
+const http = require("http");
+const socket = require("socket.io");
+const jwt = require("jsonwebtoken");
+
+const secret = process.env.SECRET || "692167";
 
 //custom routes
 const authenticationRoute = require("./routes/authentication.route");
 const messageRoute = require("./routes/messages.route");
+const { User, Message } = require("./mongo/models");
 
 // mogo db model
 
@@ -17,7 +24,62 @@ const accessLogStream = fs.createWriteStream(
   { flags: "a" }
 );
 
+// express application
 const app = express();
+
+//socket server
+const server = http.createServer(app);
+const socket_users = {}; // maping the user email with id to send private messages
+const io = new socket.Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("joined", (token) => {
+    try {
+      const { _id } = jwt.verify(token, secret);
+      socket_users[_id] = socket.id;
+    } catch (er) {
+      console.log("Wrong token is given.. So ignore the request");
+      socket.emit("wrong_token", "Wrong token is given..");
+    }
+  });
+
+  socket.on("new-anonymouse-message", async (data) => {
+    try {
+      io.to(socket_users[data.uid]).emit("new_message", data);
+      const user = await User.where({ _id: data.uid }).findOne();
+      console.log("User data => ", user);
+      if (user === null) {
+        socket.emit("response-back", {
+          status: false,
+          message: "Message failed!",
+        });
+      } else {
+        const postMessage = new Message({
+          message: data.message,
+          uid: data.uid,
+        });
+        await postMessage.save();
+        socket.emit("response-back", {
+          status: true,
+          message: "Message sent!",
+        });
+      }
+    } catch (error) {
+      console.log("socket.io => ", error);
+      socket.emit("response-back", {
+        status: false,
+        message: "Message failed!",
+      });
+    }
+  });
+});
+
+// normal express methods
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://secret-msg-test.netlify.app"],
@@ -34,6 +96,6 @@ app.get("/", (req, res) => {
   res.send("Not allowed");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`App is running in port ${port}`);
 });
